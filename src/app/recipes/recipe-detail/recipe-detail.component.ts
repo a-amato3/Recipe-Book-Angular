d70a4e91ba26deff4679eac2438299ac283258ad
Aaron Amato <amato@pnptc.com>
2020-09-28 18:14:38 +0200

    add routing

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { Ingredient } from '../../shared/ingredient.model';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+  }
+  onAddToShoppingList() {
+    this.recipeService.onAddToShoppingList(this.recipe.ingredients);
+  }
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+
+@Injectable()
+export class RecipeService {
+   recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 5)
+    ]),
+    new Recipe(
+      'Another Test', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 5),
+        new Ingredient('Meat', 3),
+
+    ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService){}
+
+  onAddToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
